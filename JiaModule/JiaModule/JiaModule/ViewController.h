7c5a6b0e074bf44266dd188da3752a7baac74dae
wujunyang <wujunyang@126.com>
2018-10-16 15:51:51 +0800

    修改文件路径

---
+++ JiaModule/JiaModule/JiaModule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JiaModule
+//
+//  Created by wujunyang on 2018/10/16.
+//  Copyright © 2018年 com.xmJiaModeule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JiaModule/JiaModule/Pods/Target Support Files/Pods-jiaModuleDefault-JiaModule/Pods-jiaModuleDefault-JiaModule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_jiaModuleDefault_JiaModule : NSObject
+@end
+@implementation PodsDummy_Pods_jiaModuleDefault_JiaModule
+@end
